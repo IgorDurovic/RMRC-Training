fb4d4e54a789db8e7f19ad316a2c8ab67ca360b6
Igor Durovic <id.tesla@gmail.com>
2017-11-06 15:15:44 -0700

    Finally solved race. All test cases passed... thank god

--- src/RMRC2015/race.java
+++ src/RMRC2015/race.java
@@ -1,98 +1,112 @@
 package RMRC2015;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class race {
-	
+
 	public static int n, T;
 	public static int[][] dist, memo;
 	public static Location[] locs;
-	
+
 	public static class Location {
 		public int p, t, d;
-		
-		Location(int a, int b, int c){
+
+		Location(int a, int b, int c) {
 			p = a;
 			t = b;
 			d = c;
 		}
 	}
-	
-	public static int solve(int bitset, int next) {
-		if(bitset == 0) return dist[n][next];
-		if(next != -1 && memo[bitset][next] != 0) return memo[bitset][next];
-		
-		int min = Integer.MAX_VALUE;
-		int prev = -1;
-		for(int i = 0; i < 21; i++) {
-			int temp = bitset;
-			if((1 << i & bitset) != 0) {
-				temp = bitset ^ (1 << i);
-				int sol = solve(temp, i);
-				if(sol != -1 && sol < min) {
+
+	public static int solve(int bitset, int next, int time) {
+		if (memo[next][bitset] >= 0)
+			return memo[next][bitset];
+		if (bitset == (1<<next)) {
+			return memo[next][bitset] = locs[next].t + dist[n][next] <= locs[next].d ? locs[next].t + dist[n][next] : 1441;
+		}
+
+		int min = 1441;
+		for (int i = 0; i < n; i++) {
+			if (i != next && (1 << i & bitset) != 0) {
+				int temp = bitset ^ (1 << next);
+				int sol = dist[i][next] + locs[next].t + solve(temp, i, time);
+				if (sol <= locs[next].d && sol < min) {
 					min = sol;
-					prev = i;
 				}
 			}
 		}
-		
-		if(next != -1) {
-			memo[bitset][next] = min + dist[prev][next]<= T ? min : -1;
-		}
-		
-		return min <= T ? min + dist[prev][n + 1] : -1;
+
+		return memo[next][bitset] = min;
 	}
-	
+
 	public static int points(int bitset) {
 		int total = 0;
-		for(int i = 0; i < n; i++) {
-			if((bitset & 1) == 1) {
+		for (int i = 0; i < n; i++) {
+			if ((bitset & (1 << i)) == 1 << i) {
 				total += locs[i].p;
 			}
-			bitset >>= 1;
 		}
-		
+
 		return total;
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter pw = new PrintWriter(System.out);
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		n = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-		
+
 		dist = new int[n + 2][n + 2];
-		memo = new int[(int)Math.pow(2, 21)][n + 2];
-		locs = new Location[n];
+		memo = new int[n][1 << n];
 		
-		for(int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			locs[i] = new Location(Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken()),
-					Integer.parseInt(st.nextToken()));
+		for(int i = 0; i < memo.length; i++) {
+			Arrays.fill(memo[i], -1);
 		}
 		
-		for(int i = 0; i < n; i++) {
+		locs = new Location[n];
+
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < n; j++) {
+			locs[i] = new Location(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken()));
+			
+			if(locs[i].d == -1) locs[i].d = 1441;
+		}
+
+		for (int i = 0; i < n + 2; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n + 2; j++) {
 				dist[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+
 		int max = 0;
 		int sol = 0;
-		for(int i = 1; i <= Math.pow(2, 21) - 1; i++) {
-			if(solve(i, -1) != -1 && points(i) > max) {
-				max = points(i);
-				sol = i;
+		for (int i = 0; i < 1 << n; i++) {
+			for (int last = 0; last < n; last++) {
+				if(((1 << last) & i) == 0) continue;
+				if (solve(i, last, 0) + dist[last][n + 1] <= T && points(i) > max) {
+					max = points(i);
+					sol = i;
+				}
+			}
+		}
+
+		pw.println(max);
+		
+		StringBuilder set = new StringBuilder();
+		for(int i = 0; i < n; i++) {
+			if((sol & (1 << i)) == 1 << i) {
+				set.append((i + 1) + " ");
 			}
 		}
 		
-		pw.println(max);
-		
+		pw.println(set.toString().trim());
+
 		pw.flush();
 		pw.close();
 		br.close();
