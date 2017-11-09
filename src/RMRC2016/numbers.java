ae2750439a4ea564521d0c183939c51e35e8fbcf
Igor Durovic <id.tesla@gmail.com>
2017-11-08 23:59:26 -0700

    solution for numbers completed. basic greedy

--- src/RMRC2016/numbers.java
+++ src/RMRC2016/numbers.java
@@ -4,76 +4,69 @@ import java.io.*;
 import java.util.Scanner;
 
 public class numbers {
-	
+
 	public static String num;
-	public static boolean fin;
-	
-	public static boolean reduce() {
-		
-		StringBuilder sb = new StringBuilder();
-		fin = true;
-		for(int i = num.length() - 1; i > 0; i -= 2) {
-			if(num.charAt(i) != '8' || num.charAt(i - 1) != '8') {
-				fin = false;
+	public static int sum;
+
+	public static void makeNicer(int n) {
+
+		StringBuilder sb = new StringBuilder(num);
+
+		if (n == 8) {
+			sum = 0;
+			for (char c : num.toCharArray()) {
+				sum += (c - '0');
 			}
-			
-			if(num.charAt(i) != '8' && num.charAt(i) == num.charAt(i - 1)) {
-				char join = (char)((num.charAt(i) - '0') + (num.charAt(i - 1) - '0') + '0');
-				sb.append(join);
-			}
-			else {
-				sb.append(num.charAt(i));
-				sb.append(num.charAt(i - 1));
-			}
-		}
-		
-		String temp = sb.reverse().toString();
-		
-		if(temp.equals(num)) {
-			return false;
-		}
-		else {
-			num = temp;
-			return true;
-		}
-	}
-	
-	public static void makeNicer() {
-		StringBuilder sb = new StringBuilder();
-		
-		if(fin) {
-			sb.append(num);
-			int bit = Integer.highestOneBit(num.length());
-			
-			if(bit != num.length()) {
-				bit <<= 1;
-			}
-			
-			for(int i = num.length(); i < bit; i++) {
+
+			while (sum != Integer.highestOneBit(sum)) {
 				sb.append('8');
+				sum += 8;
+			}
+
+			num = sb.toString();
+			return;
+		}
+
+		sum = 0;
+		for (int i = 0; i < sb.length(); i++) {
+			if (sb.charAt(i) - '0' > n) {
+				if (sum % (n * 2) != 0) {
+					sb.insert(i, n);
+					i++;
+				}
+
+				sum = 0;
+			} else {
+				sum += (sb.charAt(i) - '0');
 			}
 		}
+
+		if (sum % (n * 2) != 0) {
+			sb.append(n);
+		}
+
+		num = sb.toString();
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
 		PrintWriter out = new PrintWriter(System.out);
-		
+
 		int T = in.nextInt();
-		
-		for(int i = 0; i < T; i++) {
+
+		for (int i = 0; i < T; i++) {
 			num = in.next();
-			
-			while(num.length() != 1) {
-				while(reduce()) {}
-				
-				makeNicer();
-				
+			sum = 0;
+
+			if (!num.equals("22") && num.length() != 1) {
+				makeNicer(2);
+				makeNicer(4);
+				makeNicer(8);
 			}
-			
+
 			out.println(num);
 		}
-		
+
 		out.flush();
 		out.close();
 		in.close();
